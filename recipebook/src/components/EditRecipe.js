76f60fe654b4a52e24d4946bac859f0342f12447
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-05-21 19:43:14 -0400

    Adding in edit recipe functionality

--- recipebook/src/components/EditRecipe.js
+++ recipebook/src/components/EditRecipe.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-function EditRecipe(props) {
+function EditRecipe() {
   const [title, setTitle] = useState('');
   const [ingredients, setIngredients] = useState([]);
   const [instructions, setInstructions] = useState('');
+  
+  const { id } = useParams();
 
   useEffect(() => {
-    axios.get('http://localhost:5000/recipes/' + props.match.params.id)
+    axios.get('http://localhost:5000/recipes/' + id)
       .then(response => {
         setTitle(response.data.title);
         setIngredients(response.data.ingredients);
@@ -16,7 +19,7 @@ function EditRecipe(props) {
       .catch(function (error) {
         console.log(error);
       })
-  }, [props.match.params.id]);
+  }, [id]);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -29,7 +32,7 @@ function EditRecipe(props) {
 
     console.log(recipe);
 
-    axios.post('http://localhost:5000/recipes/update/' + props.match.params.id, recipe)
+    axios.post('http://localhost:5000/recipes/update/' + id, recipe)
       .then(res => console.log(res.data));
 
     window.location = '/';
@@ -39,7 +42,36 @@ function EditRecipe(props) {
     <div>
       <h3>Edit Recipe</h3>
       <form onSubmit={onSubmit}>
-        {/* Form fields to edit title, ingredients, and instructions */}
+        <div className="form-group"> 
+          <label>Title: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              />
+        </div>
+        <div className="form-group"> 
+          <label>Ingredients: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={ingredients}
+              onChange={e => setIngredients(e.target.value.split(','))}
+              />
+        </div>
+        <div className="form-group"> 
+          <label>Instructions: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={instructions}
+              onChange={e => setInstructions(e.target.value)}
+              />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Edit Recipe" className="btn btn-primary" />
+        </div>
       </form>
     </div>
   );
