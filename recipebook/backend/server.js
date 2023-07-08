1367efd8a6b14f0e959b1a30e883d0d96042b58e
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-07-08 16:20:17 -0400

    Trying to sort heroku deployment

--- recipebook/backend/server.js
+++ recipebook/backend/server.js
@@ -3,13 +3,17 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const recipesRouter=require('./routes/recipes');
-
+const path = require('path');
 const app=express();
 const port=process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/recipes',recipesRouter);
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 
 const uri=process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,3 +31,5 @@ app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`);
 }
 );
+
+app.listen(process.env.PORT || 8080);
